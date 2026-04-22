16be54f931aac18e32ba20c03ee95e79f28da985
Amartinez6947 <alexa.martinez@csuglobal.edu>
2026-04-22 08:21:28 -0600

    Update SelectionSort.java

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class SelectionSort {
+
+    public static void sort(ArrayList<Student> list, Comparator<Student> comp) {
+
+        for (int i = 0; i < list.size() - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < list.size(); j++) {
+
+                if (comp.compare(list.get(j), list.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+
+            // swap
+            Student temp = list.get(i);
+            list.set(i, list.get(minIndex));
+            list.set(minIndex, temp);
+        }
+    }
 }
